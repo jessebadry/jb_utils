af360d279fca1ae4ed546cee272d3e52838f9234
Jesse Badry <jessebadry@gmail.com>
2020-07-04 00:14:32 -0700

    Test123

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::stdin;
 pub fn input(msg: &str) -> String {
     let mut user_input = String::new();
+    println!("{}", msg);
     stdin().read_line(&mut user_input).unwrap_or_else(|e| {
         println!("Error from reading stdin: {}", e);
         0
