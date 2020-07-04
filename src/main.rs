6264bf27783eaf964329562e9c4879a41596432d
Jesse Badry <jessebadry@gmail.com>
2020-07-04 00:13:53 -0700

    Added pub to input func

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::stdin;
-fn input(msg: &str) -> String {
+pub fn input(msg: &str) -> String {
     let mut user_input = String::new();
     stdin().read_line(&mut user_input).unwrap_or_else(|e| {
         println!("Error from reading stdin: {}", e);
@@ -10,5 +10,5 @@ fn input(msg: &str) -> String {
 }
 fn main() {
     
-    
+
 }
