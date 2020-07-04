39ec6f6f9d054401d416220e143a4d85ad07e3e5
Jesse Badry <jessebadry@gmail.com>
2020-07-04 00:12:56 -0700

    Added input function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::stdin;
+fn input(msg: &str) -> String {
+    let mut user_input = String::new();
+    stdin().read_line(&mut user_input).unwrap_or_else(|e| {
+        println!("Error from reading stdin: {}", e);
+        0
+    });
+
+    user_input
+}
 fn main() {
-    println!("Hello, world!");
+    
+    
 }
