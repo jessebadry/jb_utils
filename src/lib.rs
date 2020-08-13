72dcca9b45ea13a264a7b9cfc500d673c9e8895d
JesseBadry <jessebadry@gmail.com>
2020-08-13 11:57:33 -0700

    Added EasyRead

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 pub mod jb_inputs;
 #[macro_use]
 pub mod j_macs;
-pub mod ffi_extensions;
+pub mod extensions;
 pub mod str_extensions;
 pub mod debug;
+
+pub enum Error{
+    ReadError(String),
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,27 +1,30 @@
-#[macro_use]
-use jb_utils::io_err;
-use jb_utils::str_extensions::StringExt;
-use super::*;
-use std::io::ErrorKind;
-//...
+#[cfg(test)]
+mod tests {
 
-#[test]
-fn make_err_pat1() {
-   let msg = "Error Message";
-   let err = io_err!(msg);
-   assert_eq!(err.kind(), ErrorKind::Other);
-   assert_eq!(err.to_string(), msg);
-}
-#[test]
-fn make_err_pat2() {
-   let msg = "Had a Already exists exception";
-   let err = io_err!(ErrorKind::AlreadyExists, msg);
-   assert_eq!(ErrorKind::AlreadyExists, err.kind());
-   assert_eq!(msg, err.to_string());
-}
-#[test]
-fn mult_string(){
-   let string = String::from("bruh");
-   assert_eq!(string.mul(3), "bruhbruhbruh".to_string());
-   println!("string = {}", string);
+   #[macro_use]
+   use jb_utils::io_err;
+   use jb_utils::str_extensions::StringExt;
+   use std::io::ErrorKind;
+   //...
+
+   #[test]
+   fn make_err_pat1() {
+      let msg = "Error Message";
+      let err = io_err!(msg);
+      assert_eq!(err.kind(), ErrorKind::Other);
+      assert_eq!(err.to_string(), msg);
+   }
+   #[test]
+   fn make_err_pat2() {
+      let msg = "Had a Already exists exception";
+      let err = io_err!(ErrorKind::AlreadyExists, msg);
+      assert_eq!(ErrorKind::AlreadyExists, err.kind());
+      assert_eq!(msg, err.to_string());
+   }
+   #[test]
+   fn mult_string() {
+      let string = String::from("bruh");
+      assert_eq!(string.mul(3), "bruhbruhbruh".to_string());
+      println!("string = {}", string);
+   }
 }
