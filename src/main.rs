fb5ceba6a84d8c4e37604ada3d044d7ce5ba74d0
JesseBadry <jessebadry@gmail.com>
2020-08-13 17:20:01 -0700

    Fixed lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-mod debug;
-use debug::PerformanceTimer;
-
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
-fn main() {
-    {
-        let timer = PerformanceTimer::new();
-        //println!("");
-    }
-
+fn main(){
+    
 }
