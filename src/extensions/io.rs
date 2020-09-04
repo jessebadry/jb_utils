50f360d0e2a24f4b47feb0e20354adebe3d99874
JesseBadry <jessebadry@gmail.com>
2020-09-04 12:34:00 -0700

    Added io changes listed in previous commit

--- src/extensions/io.rs
+++ src/extensions/io.rs
@@ -1,25 +1,22 @@
-use crate::Error;
 use std::io::Read;
+use std::io;
 pub trait EasyRead {
-    fn read_inplace(&mut self, len: usize) -> Result<Vec<u8>, Error>;
-    fn e_read(&mut self, buf: &mut [u8], read: &mut usize) -> Result<usize, Error>;
+    fn read_inplace(&mut self, len: usize) -> io::Result<Vec<u8>>;
+    fn e_read(&mut self, buf: &mut [u8], read: &mut usize) -> io::Result<usize>;
 }
 
 impl<T> EasyRead for T
 where
     T: Read,
 {
-    fn read_inplace(&mut self, len: usize) -> Result<Vec<u8>, Error> {
+    ///Reads the amount of bytes specified in the Reader.
+    fn read_inplace(&mut self, len: usize) -> io::Result<Vec<u8>>{
         let mut buf = vec![0u8; len];
-        self.read(&mut buf)
-            .map_err(|e| Error::ReadError(e.to_string()))?;
+        self.read_exact(&mut buf)?;
         Ok(buf)
     }
-    fn e_read(&mut self, buf: &mut [u8], read: &mut usize) -> Result<usize, Error> {
-        let n = self
-            .read(buf)
-            .map_err(|e| Error::ReadError(e.to_string()))?;
-        *read = n;
-        Ok(n)
+    fn e_read(&mut self, buf: &mut [u8], read: &mut usize) -> io::Result<usize> {
+        *read = self.read(buf)?;
+        Ok(*read)
     }
 }
