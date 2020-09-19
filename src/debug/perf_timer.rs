257ab79f9e809b9064a832859960f6e67caab3da
JesseBadry <jessebadry@gmail.com>
2020-09-18 18:53:32 -0700

    Added count chars to strings, and added strings implementation to &str as well

--- src/debug/perf_timer.rs
+++ src/debug/perf_timer.rs
@@ -3,30 +3,31 @@ use std::time::{SystemTime, UNIX_EPOCH};
 pub struct PerformanceTimer {
     start: u128,
 }
+
 impl PerformanceTimer {
     pub fn new() -> PerformanceTimer {
         Self::default()
     }
 }
+
+fn time_now_nanos() -> u128 {
+    let time = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_else(|e| panic!("Could not get time since epoch, why: {}", e))
+        .as_nanos();
+
+    time
+}
 impl Default for PerformanceTimer {
     fn default() -> Self {
-        let time = SystemTime::now();
-        let time = time
-            .duration_since(UNIX_EPOCH)
-            .unwrap_or_else(|e| panic!("Could not get time since epoch, why: {}", e))
-            .as_nanos();
-
-        PerformanceTimer { start: time }
+        PerformanceTimer {
+            start: time_now_nanos(),
+        }
     }
 }
 impl Drop for PerformanceTimer {
     fn drop(&mut self) {
-        let time = SystemTime::now();
-        let time = time
-            .duration_since(UNIX_EPOCH)
-            .unwrap_or_else(|e| panic!("Could not get time since epoch, why: {}", e))
-            .as_nanos();
-
+        let time = time_now_nanos();
         let time_taken = time - self.start;
         let time_taken = (time_taken as f64) / 1000000.0;
         println!("Time taken in milliseconds: {}", time_taken);
--- src/extensions/strings.rs
+++ src/extensions/strings.rs
@@ -1,17 +1,40 @@
 pub trait StringExt {
-    fn mul(&self, times: i32) -> Self;
+    fn mul(&self, times: u32) -> String;
+    fn count_char(&self, chr_bound: char) -> usize;
+}
+fn mul_impl<T: Into<String>>(string: T, times: u32) -> String {
+    let mut string_multed = string.into();
+    let copy = string_multed.clone();
+    for _ in 1..times {
+        string_multed.push_str(&copy);
+    }
+
+    string_multed
+}
+fn count_chars_impl(string: &str, chr_bound: char) -> usize {
+    let mut i = 0;
+    for chr in string.chars() {
+        if chr_bound == chr {
+            i += 1;
+        }
+    }
+    i
 }
 
-impl StringExt for String {
-    fn mul(&self, times: i32) -> Self {
-        let mut string_multed = self.clone();
-
-        for _ in 1..times {
-            string_multed.push_str(&self);
-        }
-
-        string_multed
+impl StringExt for &str {
+    fn mul(&self, times: u32) -> String {
+        mul_impl(*self, times)
+    }
+    fn count_char(&self, chr_bound: char) -> usize {
+        count_chars_impl(self, chr_bound)
     }
 }
 
-
+impl StringExt for String {
+    fn mul(&self, times: u32) -> String {
+        mul_impl(self, times)
+    }
+    fn count_char(&self, chr_bound: char) -> usize {
+        count_chars_impl(self, chr_bound)
+    }
+}
