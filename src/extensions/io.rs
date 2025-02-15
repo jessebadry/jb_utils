6d1597781af01180c131bfec4c5fafc4c4f9e919
Jesse Badry <45022943+jessebadry@users.noreply.github.com>
2025-02-15 15:09:14 -0800

    optimized easy read

--- src/extensions/io.rs
+++ src/extensions/io.rs
@@ -13,7 +13,8 @@ where
     fn read_inplace(&mut self, len: usize) -> io::Result<Vec<u8>> {
         let mut buf = vec![0u8; len];
         let r = self.read(&mut buf)?;
-        Ok(buf[..r].to_vec())
+        buf.truncate(r);  // Just truncate instead of creating new vec
+        Ok(buf)
     }
     fn e_read(&mut self, buf: &mut [u8], read: &mut usize) -> io::Result<usize> {
         *read = self.read(buf)?;
